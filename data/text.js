d398f0a1dcf858cf3c9fb2c10d18ca85368618f6
Sephalovable <kayb93@gmail.com>
2025-01-26 17:18:53 -0600

    Add text data
    Returns text data

---
+++ data/text.js
@@ -0,0 +1,8 @@
+export const predeterminedText = [
+    "You feel a strange energy surrounding you...",
+    "A glimmer of light catches your eye...",
+    "The sound of distant whispers fills the air...",
+    "A sudden chill runs down your spine...",
+    "You hear the faint clinking of treasure..."
+  ];
+  
