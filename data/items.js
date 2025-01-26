945067debb05c4d1cf6308805ff9607136659c62
Sephalovable <kayb93@gmail.com>
2025-01-26 16:51:59 -0600

    Add categories
    Add categories for multiple activities

--- data/items.js
+++ data/items.js
@@ -1,1 +1,27 @@
-export const items = ["Sword", "Shield", "Potion", "Bow", "Arrow", "Helmet", "Armor", "Ring", "Amulet", "Boots"];
+export const categories = {
+    weapons: [
+      { name: "Sword", rarity: 5 },
+      { name: "Bow", rarity: 3 }
+    ],
+    armor: [
+      { name: "Shield", rarity: 4 },
+      { name: "Helmet", rarity: 2 }
+    ],
+    potions: [
+      { name: "Healing Potion", rarity: 8 },
+      { name: "Mana Potion", rarity: 5 }
+    ],
+    rings: [
+      { name: "Ring of Strength", rarity: 6 },
+      { name: "Ring of Dexterity", rarity: 4 }
+    ],
+    amulets: [
+      { name: "Amulet of Protection", rarity: 7 },
+      { name: "Amulet of Power", rarity: 3 }
+    ],
+    misc: [
+      { name: "Rope", rarity: 10 },
+      { name: "Lantern", rarity: 7 }
+    ]
+  };
+  
